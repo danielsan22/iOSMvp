06e57580c2fdf7f33e1b7ce1da40baaf664c0acb
dsanchezpc <dsanchezpc@imac-de-daniel.local>
2018-11-20 14:27:37 -0600

    Initial Commit

---
+++ NavigationExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationExample
+//
+//  Created by dsanchezpc on 20/11/18.
+//  Copyright © 2018 dsanchezpc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
